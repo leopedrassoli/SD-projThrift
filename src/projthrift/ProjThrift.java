fc07b6e5993749ac0587ecba03beaed3f332fdaa
leopedrassoli <>
2020-11-14 15:13:37 -0200

    First commit

---
+++ src/projthrift/ProjThrift.java
@@ -0,0 +1,24 @@
+package projthrift;
+
+/**
+ *
+ * @author Leonardo Pedrassoli Silva 11521ECP003
+ */
+import chavevalor.*;
+import org.apache.thrift.TException;
+
+public class ProjThrift {
+
+    public static void main(String[] args) throws TException {
+        ChaveValorHandler handler = new ChaveValorHandler();
+        
+        //Inserindo chaves corretas
+        handler.setKV(0, "a");
+        handler.setKV(1, "b");
+        handler.setKV(2, "c");
+        
+        //Inserindo chave duplicada
+        handler.setKV(2, "c");
+    }
+    
+}
